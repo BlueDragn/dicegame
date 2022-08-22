fd3dd994bb8ee90eb9b444137c3aa5a29fdaf59f
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2022-08-22 15:31:34 +0530

    fixing some typo error

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 
 let z = ["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
-let x = Math.round((Math.random() * z.length)+1);
-let y = Math.round((Math.random() * z.length)+1);
+let x = Math.floor((Math.random() * z.length)+1);
+let y = Math.floor((Math.random() * z.length)+1);
 
 
 
-document.getElementById("img1").setAttribute("src",z[x-1]);
-document.getElementById("img2").setAttribute("src",z[y-1]);
+document.querySelector(".img1").setAttribute("src",z[x-1]);
+document.querySelector(".img2").setAttribute("src",z[y-1]);
+
+
+
+   if(x>y){
+    document.querySelector("h1").innerHTML = "&#128681 player 1 Wins";
+  }
+  else if (x<y) {
+    document.querySelector("h1").innerHTML ="player 2 Wins &#128681";
+  }
+  else{
+    document.querySelector("h1").innerHTML ="	&#127987 Draw 	&#127987";
+  }
